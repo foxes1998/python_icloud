1b621293588071a5e5783c8244733d6c655db690
ShimaTwo <itfs1311368@icloud.com>
2019-03-19 15:46:09 +0900

    バグ修正、新規画像に対するakazeマッチングチェックで新規画像自身とマッチングしてしまうバグがまだある

--- pyicloud_photo_download.py
+++ pyicloud_photo_download.py
@@ -67,6 +67,8 @@ if __name__ == '__main__':
     # オリジナルのメガネ画像保存先パス
     glasses_original_dir_pass = '/Users/shimadatakuyume/iCloud_pi/icloud_photos/glasses/glasses_original/'
     glasses_original_dir_filenum = listdir_nohidden(glasses_original_dir_pass)
+    # インスタンスに画像が保存されているパスを与える
+    ins_check_daplication = CD.check_daplication(glasses_original_dir_pass)
     # glasses_original_dir_filenum = len(glasses_original_dir_files)
     print('glasses_original_dir_filenum:'+ str(glasses_original_dir_filenum))
     icloud_glasses_photo_number = len(api.photos.albums['glasses'])
@@ -82,15 +84,14 @@ if __name__ == '__main__':
                 root, ext = os.path.splitext(photo.filename)
                 with open('/Users/shimadatakuyume/iCloud_pi/icloud_photos/glasses/glasses_original/'+ str(photo_number) + str(ext), 'wb') as opened_file:
                     opened_file.write(download.raw.read())
-                dap_pair = ins_check_daplication.check_daplication_for_single_file('/Users/shimadatakuyume/iCloud_pi/icloud_photos/glasses/glasses_original/'+ str(photo_number+1) + str(ext))
+                dap_pair = ins_check_daplication.check_daplication_for_single_file('/Users/shimadatakuyume/iCloud_pi/icloud_photos/glasses/glasses_original/'+ str(photo_number) + str(ext))
                 photo_number = photo_number-1
-                # インスタンスに画像が保存されているパスを与える
-                ins_check_daplication = CD.check_daplication(glasses_original_dir_pass)
-                if photo_number == glasses_original_dir_filenum:
-                    break
-                elif dap_pair != []:
+                if dap_pair != []:
                     print('There is daplication file(s)')
                     print(dap_pair)
+                if photo_number == glasses_original_dir_filenum:
+                    break
+                
     else:
         print('no update')
 
